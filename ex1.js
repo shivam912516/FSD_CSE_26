0725cd6c248dd28af7e9df9d60bafa7f93729ff3
shivam912516 <mauryashivam912516@gmail.com>
2026-07-28 10:27:04 +0530

    demo let and const ,arrow, map, filter, reduce

---
+++ ex1.js
@@ -0,0 +1,3 @@
+var a=10;
+a=20;
+console.log("a=",a)
---
+++ ex2.js
@@ -0,0 +1,13 @@
+// function sum(a, b) {
+//     return (a + b);
+// }
+// console.log("sum=",sum(12))
+// console.log("sum=",sum(12, 13))
+// console.log("sum=",sum())
+
+const sum = (a, b) => {
+    return (a + b);
+}
+console.log("sum=",sum(12))
+console.log("sum=",sum(12, 13))
+console.log("sum=",sum())
